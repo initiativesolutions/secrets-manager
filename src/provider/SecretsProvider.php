63fd5a82ac0a51e2efcfdcd010eaafb968de053e
amine <a.bougassaa@abo-s.com>
2023-08-09 11:30:40 +0200

    feat: finish provider

--- src/provider/SecretsProvider.php
+++ src/provider/SecretsProvider.php
@@ -3,6 +3,7 @@
 namespace SecretsManager\Provider;
 
 use SecretsManager\FileAccess\ReadFiles;
+use SecretsManager\SecretsConfig;
 
 class SecretsProvider
 {
@@ -11,8 +12,12 @@ class SecretsProvider
      * @param string $secretKeyPath path encrypted key
      * @param string $secretsTokensPath path to secrets json tokens
      */
-    public function decrypt(string $secretKeyPath, string $secretsTokensPath): array
+    public function decrypt(string $secretKeyPath, string $secretsTokensPath, string $algo = null): array
     {
+        if (is_null($algo)) {
+            $algo = SecretsConfig::get('encrypt.algorithm');
+        }
+
         $secretKey = (new ReadFiles())
             ->setFilePath($secretKeyPath)
             ->read();
@@ -21,9 +26,12 @@ class SecretsProvider
             ->setFilePath($secretsTokensPath)
             ->readJson();
 
-        // todo change, just for test
-
-        return [$secretKey, $secretTokens];
+        return array_map(function ($value) use ($secretKey, $algo) {
+            $decodedValue = base64_decode($value);
+            $iv = substr($decodedValue, 0, 16);
+            $encryptedValue = substr($decodedValue, 16);
+            return openssl_decrypt($encryptedValue, $algo, hex2bin($secretKey), OPENSSL_RAW_DATA, $iv);
+        }, $secretTokens);
     }
 
 }
