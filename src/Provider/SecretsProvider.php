11e72a26e036e1f5e078689e5089f9237e4e2e06
amine <a.bougassaa@abo-s.com>
2023-08-29 16:00:50 +0200

    fix: set default algo for decrypt

--- src/Provider/SecretsProvider.php
+++ src/Provider/SecretsProvider.php
@@ -12,7 +12,7 @@ class SecretsProvider
      * @param string $secretKeyPath path encrypted key
      * @param string $secretsTokensPath path to secrets json tokens
      */
-    public function decryptByFiles(string $secretKeyPath, string $secretsTokensPath, string $algo = null): array
+    public function decryptByFiles(string $secretKeyPath, string $secretsTokensPath, string $algo = 'aes256'): array
     {
         $secretKey = (new FileAccess($secretKeyPath))
             ->read();
@@ -23,7 +23,7 @@ class SecretsProvider
         return $this->decryptByTokens($secretKey, $secretTokens, $algo);
     }
 
-    public function decryptByTokens(string $secretKey, array $secretsTokens, string $algo = null): array
+    public function decryptByTokens(string $secretKey, array $secretsTokens, string $algo = 'aes256'): array
     {
         return array_map(function ($value) use ($secretKey, $secretsTokens, $algo) {
             return Guard::decryptValue($secretKey, $value, $algo);
